9a235fc73adc4f6f21f5c5044be867f55ab7f684
speedpotato-25 <aarav.gupta.ceo@gmail.com>
2026-01-28 18:46:46 +0530

    works now =)

--- main.py
+++ main.py
@@ -10,17 +10,11 @@ r = sr.Recognizer()
 time = None
 response = None
 answer = None
+r = sr.Recognizer()
 
-while True:
-    with sr.Microphone() as source:
-        print("Adjusting for ambient noise... Please wait.")
-        r.adjust_for_ambient_noise(source, duration=1)
-        print("Okay, say something!")
-
-        # Listen for the user's input
-        audio = r.listen(source)
-
-    # 3. Recognize the speech
+def main_part(source):
+    print("starting")
+    audio = r.listen(source, phrase_time_limit=3)
     try:
         print("Recognizing...")
         text = r.recognize_google(audio)
@@ -31,9 +25,36 @@ while True:
         engine.say(answer)
         engine.runAndWait()
 
+    except sr.UnknownValueError:
+        engine.say("Sorry, I cannot understand")
+        engine.runAndWait()
+
     except Exception as e:
         print("error")
 
     finally:
         print("done")
 
+def wake_up():
+    with sr.Microphone() as source:
+        print("Adjusting for ambient noise... Please wait.")
+        r.adjust_for_ambient_noise(source,duration= 2)
+        print("adjustment done")
+
+        while True:
+            try:
+                audio = r.listen(source, timeout=1, phrase_time_limit=3)
+                audio_text = r.recognize_google(audio).lower()
+
+                if "hello" in audio_text:
+                    engine.say("how may i help you today")
+                    engine.runAndWait()
+                    main_part(source)
+            except (sr.UnknownValueError, sr.WaitTimeoutError):
+                continue
+
+            except Exception as e:
+                print(f"error{e}")
+
+
+wake_up()
